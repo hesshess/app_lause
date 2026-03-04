2b8c0a5aa99475206580ee8de61323cfff455726
Hess <hess90wang@gmail.com>
2026-03-04 20:23:11 +0900

    feat: render daily leaderboard and list

--- app/features/applauses/pages/daily-leaderboard-page.tsx
+++ app/features/applauses/pages/daily-leaderboard-page.tsx
@@ -1,24 +1,67 @@
+import { DateTime } from "luxon";
 import type { Route } from "./+types/daily-leaderboard-page";
+import { data, isRouteErrorResponse } from "react-router";
+import z from "zod";
 
-export function loader(_args: Route.LoaderArgs) {
-    return {};
+const paramsSchema = z.object({
+    year: z.coerce.number(),
+    month: z.coerce.number(),
+    day: z.coerce.number(),
+})
+
+export const loader = ({params}: Route.LoaderArgs) => {
+    const { success, data: parsedData } = paramsSchema.safeParse(params);
+    if(!success){
+        throw data(
+            {
+                error_code: "invalid_params",
+                message: "Invalid params",
+            },
+            {status: 400}
+        )
+    }
+
+    const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+    if (!date.isValid){
+        throw data(
+            {
+                error_code: "invalid_date",
+                message: "Invalid date",
+            },
+            {
+                status:400
+            }
+        )
+    }
+    const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+    if(date > today){
+        throw data(
+            {
+                error_code: "future_date",
+                message: "Future date",
+            },
+            { status: 400 }
+        )
+    }
+    return{
+        date,
+    }
 }
 
-export function action(_args: Route.ActionArgs) {
-    return {};
-}
-
-export const meta: Route.MetaFunction = () => {
-    return [
-        { title: "daily leaderboard page | app_lause" },
-        { name: "description", content: "daily leaderboard page" },
-    ];
-};
-
-export default function Page(_props: Route.ComponentProps) {
+export default function DailyLeaderboardPage({loaderData}: Route.ComponentProps) {
     return (
         <div className="px-20 py-10">
             <h1 className="text-3xl font-bold capitalize">daily leaderboard page</h1>
         </div>
     );
+}
+
+export function ErrorBoundary({error}: Route.ErrorBoundaryProps){
+    if(isRouteErrorResponse(error)){
+        return <div>{error.data.message} / {error.data.error_code}</div>
+    }
+    if (error instanceof Error){
+        return <div>{error.message}</div>;
+    }
+    return <div>Unknown error</div>;
 }

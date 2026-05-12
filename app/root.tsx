d321337d0eaddfac5265bf0ce28c57b0391d30cd
Hess <hess90wang@gmail.com>
2026-05-12 22:10:46 +0900

    feat: implement auth action handling

--- app/root.tsx
+++ app/root.tsx
@@ -6,12 +6,15 @@ import {
   Scripts,
   ScrollRestoration,
   useLocation,
+  useNavigation,
 } from "react-router";
 
 import type { Route } from "./+types/root";
 import "./app.css";
 import Navigation from "./common/components/navigation";
 import { Settings } from "luxon";
+import { makeSSRClient } from "./supa-client";
+import { cn } from "./lib/utils";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -46,19 +49,29 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  const { client } = makeSSRClient(request);
+  const {
+    data: { user },
+  } = await client.auth.getUser();
+  return { user };
+};
+
+export default function App({ loaderData }: Route.ComponentProps) {
   const { pathname } = useLocation();
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  const isLoggedIn = loaderData.user !== null;
   return (
     <div
-      className={
-        pathname.includes("/auth/")
-          ? ""
-          : "px-4 py-24 sm:px-6 lg:px-12 xl:px-20"
-      }
+      className={cn({
+        "py-28 px-5 md:px-20": !pathname.includes("/auth/"),
+        "transition-opacity animate-pulse": isLoading,
+      })}
     >
       {pathname.includes("/auth") ? null : (
         <Navigation
-          isLoggedIn={true}
+          isLoggedIn={isLoggedIn}
           hasNotifications={true}
           hasMessages={true}
         />

be289003b74fc446888647805667bd593bb67eea
Hess <hess90wang@gmail.com>
2026-05-14 22:41:30 +0900

    feat: add create post page

--- app/features/users/queries.ts
+++ app/features/users/queries.ts
@@ -1,6 +1,7 @@
 import type { Database } from "~/supa-client";
 import { applauseListSelect } from "../applauses/queries";
 import type { SupabaseClient } from "@supabase/supabase-js";
+import { redirect } from "react-router";
 
 export const getUserProfile = async (
   client: SupabaseClient<Database>,
@@ -79,4 +80,12 @@ export const getUserPosts = async (
     throw error;
   }
   return data;
+};
+
+export const getLoggedInUserId = async (client: SupabaseClient<Database>) => {
+  const { data, error } = await client.auth.getUser();
+  if (error || data.user === null) {
+    throw redirect("/auth/login");
+  }
+  return data.user.id;
 };

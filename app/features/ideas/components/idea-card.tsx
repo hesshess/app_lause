932b941d4a727c6affec2f157124f1dcbd3cffd4
Hess <hess90wang@gmail.com>
2026-05-15 22:24:27 +0900

    fix: correct idea card behavior

--- app/features/ideas/components/idea-card.tsx
+++ app/features/ideas/components/idea-card.tsx
@@ -1,4 +1,5 @@
 import { DotIcon, EyeIcon, HeartIcon, LockIcon } from "lucide-react";
+import { DateTime } from "luxon";
 import { Link } from "react-router";
 
 import { Button } from "~/common/components/ui/button";
@@ -39,7 +40,7 @@ export function IdeaCard({
                     <span>{viewsCount}</span>
                 </div>
                 <DotIcon className="w-4 h-4" />
-                <span>{postedAt}</span>
+                <span>{DateTime.fromISO(postedAt).toRelative()}</span>
             </CardContent>
             <CardFooter className="flex justify-end gap-2">
                 <Button variant="outline">

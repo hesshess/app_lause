40e66733d2374483686322d3247f09d0899cd6f2
Hess <hess90wang@gmail.com>
2026-06-15 15:28:00 +0900

    feat: add optimistic UI updates

--- app/features/community/pages/upvote-post-page.tsx
+++ app/features/community/pages/upvote-post-page.tsx
@@ -1,9 +1,15 @@
-import { Route } from "./+types/upvote-post-page";
+import { makeSSRClient } from "~/supa-client";
+import { getLoggedInUserId } from "~/features/users/queries";
+import { toggleUpvote } from "../mutations";
+import type { Route } from "./+types/upvote-post-page";
 
 export const action = async ({ request, params }: Route.ActionArgs) => {
-  const formData = await request.formData();
-  const postId = formData.get("postId");
-  console.log(postId, "fetcher submit says");
+    if (request.method !== "POST") {
+    throw new Response("Method not allowed", { status: 405 });
+  }
+  const { client } = await makeSSRClient(request);
+  const userId = await getLoggedInUserId(client);
+  await toggleUpvote(client, { postId: params.postId, userId });
   return {
     ok: true,
   };

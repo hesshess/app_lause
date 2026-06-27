800cd21b916e6c7280c917984d931b578a058340
Hess <hess90wang@gmail.com>
2026-06-27 22:54:07 +0900

    feat: implement payment request flow

--- app/features/applauses/pages/promote-page.tsx
+++ app/features/applauses/pages/promote-page.tsx
@@ -4,7 +4,7 @@ import { Form } from "react-router";
 import SelectPair from "~/common/components/select-pair";
 import { Calendar } from "~/common/components/ui/calendar";
 import { Label } from "~/common/components/ui/label";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { DateRange } from "react-day-picker";
 import { DateTime } from "luxon";
 import { Button } from "~/common/components/ui/button";
@@ -13,6 +13,10 @@ import {
   getApplausesByUserId,
   getLoggedInUserId,
 } from "~/features/users/queries";
+
+import { loadTossPayments } from "@tosspayments/tosspayments-sdk";
+
+import type {TossPaymentsWidgets} from "@tosspayments/tosspayments-sdk";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -45,15 +49,72 @@ export default function PrmotePage({ loaderData }: Route.ComponentProps) {
           "days",
         ).days
       : 0;
+  const widgets = useRef<TossPaymentsWidgets | null>(null);
+  const initedToss = useRef<boolean>(false);
+  useEffect(() => {
+    const initToss = async () => {
+      if (initedToss.current) return;
+      initedToss.current = true;
+      const toss = await loadTossPayments(
+        import.meta.env.VITE_TOSS_CLIENT_KEY!,
+      );
+      widgets.current = await toss.widgets({
+        customerKey: "1111111",
+      });
+      await widgets.current.setAmount({
+        value: 0,
+        currency: "KRW",
+      });
+      await widgets.current.renderPaymentMethods({
+        selector: "#toss-payment-methods",
+      });
+      await widgets.current.renderAgreement({
+        selector: "#toss-payment-agreement",
+      });
+    };
+    initToss();
+  }, []);
+  useEffect(() => {
+    const updateAmount = async () => {
+      if (widgets.current) {
+        await widgets.current.setAmount({
+          value: totalDays * 1000,
+          currency: "KRW",
+        });
+      }
+    };
+    updateAmount();
+  }, [promotionPeriod]);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const product = formData.get("product") as string;
+    if (!product || !promotionPeriod?.to || !promotionPeriod?.from) return;
+    await widgets.current?.requestPayment({
+      orderId: crypto.randomUUID(),
+      orderName: `Applause Promotion`,
+      customerEmail: "heisuewang@gmail.com",
+      customerName: "Hess",
+      customerMobilePhone: "01012345678",
+      metadata: {
+        product,
+        promotionFrom: DateTime.fromJSDate(promotionPeriod.from).toISO(),
+        promotionTo: DateTime.fromJSDate(promotionPeriod.to).toISO(),
+      },
+      successUrl: `${window.location.href}/success`,
+      failUrl: `${window.location.href}/fail`,
+    });
+  };
   return (
     <div>
       <Hero
         title="Promote Your Applause"
         description="Highlight your progress so more people can discover and try it."
       />
-      <div className="grid grid-cols-6">
-        <Form className="col-span-4 mx-auto flex flex-col gap-10 items-center">
+      <form onSubmit={handleSubmit} className="grid grid-cols-6 gap-10">
+        <div className="col-span-3 mx-auto w-1/2 flex flex-col gap-10 items-start">
           <SelectPair
+            required
             label="Select an applause"
             description="Select the applause you want to promote"
             name="applause"
@@ -77,12 +138,20 @@ export default function PrmotePage({ loaderData }: Route.ComponentProps) {
               disabled={{ before: new Date() }}
             />
           </div>
-          <Button disabled={totalDays === 0}>
-            Go to checkout (${totalDays * 20})
+        </div>
+        <aside className="col-span-3 px-20 flex flex-col items-center">
+          <div id="toss-payment-methods" className="w-full" />
+          <div id="toss-payment-agreement" />
+          <Button className="w-full" disabled={totalDays === 0}>
+            Checkout (
+            {(totalDays * 1000).toLocaleString("ko-KR", {
+              style: "currency",
+              currency: "KRW",
+            })}
+            )
           </Button>
-        </Form>
-        <aside className="col-span-2"></aside>
-      </div>
+        </aside>
+      </form>
     </div>
   );
 }
